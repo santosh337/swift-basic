c91b695d5fd3b6cda776f8d6eac62a8a6555e6bf
Santosh Sugur <santosh.iosdev@gmail.com>
2023-06-09 18:01:54 +0000

    Create pattern 0 to 5.swift
    //1 0 0 0 0
    //0 2 0 0 0
    //0 0 3 0 0
    //0 0 0 4 0
    //0 0 0 0 5

---
+++ pattern 0 to 5.swift
@@ -0,0 +1,13 @@
+for i in 1...5 {
+    for j in 1...5 {
+
+        if j == i{
+            print(j,terminator : " ")
+        }
+        else{
+            print(0, terminator: " ")
+        }
+
+    }
+    print(" ")
+}
